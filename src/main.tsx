9e820c582680e2fd3ad214a6d1880f0c653b4e2f
Mila <malaksumaaa@gmail.com>
2025-11-03 15:16:53 +0100

    add component app und cafeinfo

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,9 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-// main.tsx
-
-import { createRoot } from "react-dom/client";
-
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <div>
-    <h1>Welcome to React</h1>
-    <p>
-      This is JSX — it looks like HTML, but it's not quite the same. It has its
-      own rules!
-    </p>
-  </div>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
