ee27e1bac6d98e1133fcf84e19bbe230c5cfdab4
Mila <malaksumaaa@gmail.com>
2025-11-03 22:55:50 +0100

    rename types folder

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import CafeInfo from '../CafeInfo/CafeInfo';
 
 import css from './App.module.css';
-import type { Votes, VoteType } from '../gggggg/votes';
+import type { Votes, VoteType } from '../types/votes';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
 import Notification from '../Notification/Notification';
